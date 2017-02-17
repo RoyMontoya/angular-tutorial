0e92297c4a0079fd045f54370d7b7e318dcecca4
RoyMontoya <roy_a18@hotmail.com>
2017-02-17 11:21:27 -0700

    highlight textbox when clicked

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
-  template: `<div>
-    <input #myInput type="text" [(ngModel)]="message">
-    <button (click)="update.emit({text:message})">Click me!</button>
-  </div>`,
-  styles: []
+  template: `
+  <input
+  #myInput
+  type="text"
+  [(ngModel)]="message"
+  [ngClass]="{mousedown:isMouseDown}"
+  (mousedown)="isMouseDown = true"
+  (mouseup)="isMouseDown = false"
+  (mouseleave)="isMouseDown = false"
+  >
+  <button (click)="update.emit({text:message})">Click me!</button>
+  `,
+  styles: [':host{display: flex; flex-direction: column;} input: focus{font-weight: bold; outline: none;} .mousedown{border: 2px solid green} button{border: none;}']
 })
 export class SimpleFormComponent implements OnInit {
 
-@Input() message;
+  @Input() message;
 
-@Output() update = new EventEmitter();
+  @Output() update = new EventEmitter();
+
+  isMouseDown;
 
   constructor() {
 
